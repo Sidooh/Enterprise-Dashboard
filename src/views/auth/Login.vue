93b87dace564019e7cc1c5f59b72f038d9d9b222
Nabcellent <nabcellent.dev@gmail.com>
2022-10-31 11:02:39 +0300

    Redesigns Login Page
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,7 +1,102 @@
+<template>
+    <div class="row d-flex justify-content-center align-items-center min-vh-100 pb-6">
+        <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 position-relative">
+            <a class="d-flex align-items-center justify-content-center mb-4" href="/">
+                <img class="me-2" src="/sidooh.png" alt="" width="100">
+            </a>
+
+            <article>
+                <header class="shadow-sm">
+                    <div class="d-flex">
+                        <div class="progress-step" :class="{'active':i === state.activeStep}"
+                             v-for="(step, i) in state.formSteps" :key="`step-${i}`">
+                            <span>{{ String(i + 1).padStart(2, '0') }}</span>
+                            <div class="position-absolute title">{{ step.title }}</div>
+                        </div>
+                    </div>
+                </header>
+
+                <section class="card shadow-sm" :class="state.animation">
+                    <div class="card-body p-4 p-sm-5 d-flex flex-column justify-content-center align-items-center">
+                        <h2>{{ state.formSteps[state.activeStep].title }}</h2>
+
+                        <div class="input-fields w-100">
+                            <div class="mb-3" v-for="(field, i) in state.formSteps[state.activeStep].fields"
+                                 :key="`field-${i}`">
+                                <input v-if="field.type !== 'file'" :type="field.type ?? 'text'"
+                                       v-model="field.value" class="form-control" :class="{'is-invalid': !field.valid}"
+                                       :placeholder="field.label" required>
+                            </div>
+                            <div class="mb-3" v-for="(f, i) in state.formSteps[state.activeStep].fields">
+                                <file-pond :key="`file-field-${i}`" v-if="f?.type === 'file'" v-model="f.value"
+                                           label-idle="Drop company registration letter here..." ref="pond"
+                                           accepted-file-types="application/pdf, application/msword"/>
+                            </div>
+                        </div>
+
+                        <div class="mt-3 align-self-end">
+                            <button class="btn btn-sm btn-outline-secondary" @click="prevStep"
+                                    v-if="state.activeStep > 0 && state.activeStep <= state.formSteps.length - 2">
+                                <font-awesome-icon :icon="faLeftLong" class="me-2"/>
+                                Back
+                            </button>
+                            <button class="btn btn-sm btn-primary ms-2" @click="nextStep"
+                                    v-if="state.activeStep + 1 < state.formSteps.length - 1">Proceed
+                                <font-awesome-icon :icon="faRightLong" class="ms-2"/>
+                            </button>
+                            <button class="btn btn-sm btn-primary ms-2" @click="nextStep"
+                                    v-if="state.activeStep + 1 === state.formSteps.length - 1">
+                                Finish
+                                <font-awesome-icon :icon="faUserPlus" class="ms-1"/>
+                            </button>
+                        </div>
+
+                        <div class="col-auto fs--1 text-600">
+                            <small>Already registered? </small>
+                            <small>
+                                <router-link :to="{name:'login'}">Sign In</router-link>
+                            </small>
+                        </div>
+                    </div>
+                </section>
+
+                <div class="position-relative mt-4">
+                    <hr/>
+                    <div class="text-center">🌟</div>
+                </div>
+
+                <div class="text-center">
+                    <i><small class="text-center opacity-75">Sidooh, Makes You Money with Every Purchase!</small></i>
+                </div>
+            </article>
+        </div>
+    </div>
+</template>
+
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import { useAuthStore } from "@/stores/auth";
 import router from "@/router";
+
+const state = reactive({
+    activeStep: 0,
+    animation: 'animate-in',
+    formSteps: [
+        {
+            title: 'Sign In',
+            fields: [
+                { type: 'email', label: 'Email address', value: '', valid: true, pattern: /.+/ },
+                { type: 'password', label: 'Password', value: '', valid: true, pattern: /.+/ },
+            ]
+        },
+        {
+            title: 'Verification',
+            fields: [
+                { label: 'Phone verification OTP', value: '', valid: true, pattern: /.+/ },
+            ]
+        }
+    ]
+})
 
 const email = ref()
 const password = ref()
@@ -34,64 +129,4 @@ const submit = () => {
     }
 }
 </script>
-
-<template>
-    <div class="row d-flex justify-content-center align-items-center min-vh-100 pb-6">
-        <div class="col-sm-10 col-md-8 col-lg-6 col-xl-5 col-xxl-4 position-relative">
-            <a class="d-flex align-items-center justify-content-center mb-4" href="/">
-                <img class="me-2" src="/sidooh.png" alt="" width="100">
-            </a>
-            <div class="card shadow-sm">
-                <div class="card-body p-4 p-sm-5">
-                    <div class="row align-items-center justify-content-between mb-2">
-                        <div class="col-auto"><h5>Sign In</h5></div>
-                        <div class="col-auto fs-6 text-600">
-                            <small>or <router-link :to="{name:'register'}">Create an account</router-link></small>
-                        </div>
-                    </div>
-                    <form id="sign-in">
-                        <div class="mb-3">
-                            <input class="form-control" type="email"
-                                   placeholder="Email address" aria-label="" required
-                                   v-model="email">
-                            <small class="form-control-sm text-danger" v-show="emailError">Invalid email address</small>
-                        </div>
-                        <div class="mb-3">
-                            <input class="form-control" type="password" placeholder="Password" aria-label="" required
-                                   v-model="password">
-                            <span class="form-control-sm alert-danger"
-                                  v-show="passwordError">min password length: 8</span>
-                        </div>
-                        <div class="row align-items-center justify-content-between">
-                            <div class="col-auto">
-                                <span class="form-control-sm alert-danger" v-show="invalidCredentials">Invalid Credentials</span>
-                            </div>
-                            <div class="col-auto">
-                                <a class="fs--1" href="/password/reset">Forgot Password?</a>
-                            </div>
-                        </div>
-                        <div class="d-flex justify-content-center mt-3">
-                            <button type="submit" class="col btn btn-sm btn-primary ld-ext-right"
-                                    @click.prevent="submit">
-                                Sign In
-                                <span class="ld ld-ring ld-spin"></span>
-                            </button>
-                        </div>
-                    </form>
-
-                    <div class="position-relative mt-4">
-                        <hr/>
-                        <div class="text-center">🌟</div>
-                    </div>
-
-                    <div class="text-center">
-                        <i>
-                            <small class="text-center opacity-75">Sidooh, Makes You Money with Every Purchase!</small>
-                        </i>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</template>
 
