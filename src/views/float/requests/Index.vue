b37c8fa46a7a7723fab9107c9af58927b3ff334e
Nabcellent <nabcellent.dev@gmail.com>
2022-11-13 13:41:18 +0300

    Implements forms
    Signed-off-by: Nabcellent <nabcellent.dev@gmail.com>

--- src/views/float/requests/Index.vue
+++ src/views/float/requests/Index.vue
@@ -1,9 +1,29 @@
 <template>
     <div class="card">
         <div class="card-body">
-            <DataTable title="Float Requests" :columns="columns" :data="tableData"/>
+            <DataTable title="Float Requests" :columns="columns" :data="tableData" :on-create-row="handleCreateRow"/>
         </div>
     </div>
+
+    <FormKit type="form" submit-label="Create" :actions="false" #default="{ state: { valid } }"
+             @submit="submitNewFloatRequest">
+        <Modal id="create-float-request">
+            <template #title>Create New Float Request</template>
+            <template #body>
+                <FormKit type="number" min="1" name="amount" step="1" placeholder="Enter float amount"
+                         :classes="{input:'form-control', outer:'mb-3'}" validation="required"/>
+                <FormKit type="textarea" name="reason" placeholder="Enter float request reason..."
+                         :classes="{input:'form-control', outer:'mb-3'}" validation=""/>
+            </template>
+            <template #footer>
+                <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                <FormKit type="submit" input-class="btn btn-primary" :disabled="!valid">
+                    Create
+                    <font-awesome-icon :icon="faCloudversify" class="ms-1"/>
+                </FormKit>
+            </template>
+        </Modal>
+    </FormKit>
 </template>
 
 <script setup lang="ts">
@@ -12,13 +32,18 @@ import StatusBadge from "@/components/StatusBadge.vue";
 import TableDate from "@/components/TableDate.vue";
 import { CellContext, createColumnHelper } from "@tanstack/vue-table";
 import { currencyFormat } from "@/utils/helpers";
-import { h } from "vue";
+import { h, onMounted, reactive } from "vue";
 import { Status } from "@/utils/enums";
 import { RouterLink } from "vue-router";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { faEye } from "@fortawesome/free-regular-svg-icons";
-import { FloatRequest } from "@/utils/types";
+import { FloatRequest, VoucherType } from "@/utils/types";
 import moment from "moment";
+import Modal from "@/components/Modal.vue";
+import { Modal as BSModal } from "bootstrap";
+import { FormKitNode } from "@formkit/core";
+import { faCloudversify } from '@fortawesome/free-brands-svg-icons'
+
 
 const columnHelper = createColumnHelper<FloatRequest>()
 const columns = [
@@ -47,7 +72,6 @@ const columns = [
         )
     },
 ]
-
 const tableData: FloatRequest[] = [
     {
         id: 1,
@@ -71,6 +95,35 @@ const tableData: FloatRequest[] = [
         updated_at: moment().subtract(1, 'h').toISOString()
     },
 ]
+
+const state = reactive<{ modal?: BSModal }>({
+    modal: undefined
+})
+
+onMounted(() => {
+    state.modal = new BSModal('#create-float-request', {})
+})
+
+const handleCreateRow = () => {
+    console.log('weee')
+
+    state.modal?.show()
+}
+
+const submitNewFloatRequest = async (formData: VoucherType, node?: FormKitNode) => {
+    try {
+        node?.clearErrors()
+
+        console.log(formData)
+
+        await new Promise((r) => setTimeout(r, 2000))
+
+        state.modal?.hide()
+        node?.reset()
+    } catch (err) {
+        console.log(err)
+    }
+}
 </script>
 
 <style scoped>
